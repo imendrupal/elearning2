5b2f241943829fb02d5c778236c6637811518e24
imen <imen.chekir@gmail.com>
2016-11-08 17:18:40 +0100

    liste cours par categorie

---
+++ sites/default/files/php/twig/722bf04f_inline-template_4699716b7cb6d566fddfbf984a9415e3dfbfcb4356762f35758ff051c5ddf17e/5d97e845c3bec41b0d2087ba13a9297860cde7a8cb4d733a7151c5928a59bae2.php
@@ -0,0 +1,62 @@
+<?php
+
+/* {# inline_template_start #}Liste des cours de {{ arguments.field_catego_target_id }} */
+class __TwigTemplate_00be9531b9f6fab064362a60109615da59768dfc02cd657b350947c59811816d extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array();
+        $filters = array();
+        $functions = array();
+
+        try {
+            $this->env->getExtension('sandbox')->checkSecurity(
+                array(),
+                array(),
+                array()
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setTemplateFile($this->getTemplateName());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        // line 1
+        echo "Liste des cours de ";
+        echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, $this->getAttribute((isset($context["arguments"]) ? $context["arguments"] : null), "field_catego_target_id", array()), "html", null, true));
+    }
+
+    public function getTemplateName()
+    {
+        return "{# inline_template_start #}Liste des cours de {{ arguments.field_catego_target_id }}";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  43 => 1,);
+    }
+}
+/* {# inline_template_start #}Liste des cours de {{ arguments.field_catego_target_id }}*/
---
+++ sites/default/files/php/twig/722bf04f_inline-template_ce8a59eeaa408e536e0892cbff8c788e9df69748a81add6b024aa025ed378a8f/8fcd713a1ced7da62b91ff21a7b1c3d6601111aec99f5e761e16cb231c81afae.php
@@ -0,0 +1,62 @@
+<?php
+
+/* {# inline_template_start #}/liste-cours-par-cat-gorie/{{ nid }} */
+class __TwigTemplate_c112dba56a3ce967bafcae011d10355af22693d943c461c90692763c4c163bcc extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array();
+        $filters = array();
+        $functions = array();
+
+        try {
+            $this->env->getExtension('sandbox')->checkSecurity(
+                array(),
+                array(),
+                array()
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setTemplateFile($this->getTemplateName());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        // line 1
+        echo "/liste-cours-par-cat-gorie/";
+        echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, (isset($context["nid"]) ? $context["nid"] : null), "html", null, true));
+    }
+
+    public function getTemplateName()
+    {
+        return "{# inline_template_start #}/liste-cours-par-cat-gorie/{{ nid }}";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  43 => 1,);
+    }
+}
+/* {# inline_template_start #}/liste-cours-par-cat-gorie/{{ nid }}*/
